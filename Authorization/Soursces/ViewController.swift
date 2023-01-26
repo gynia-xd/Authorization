e6308d456d6050d225612e27cd196b8fea3639b1
Gynia_xd <Gynia_xd@mail.ru>
2023-01-26 12:34:23 +0300

    add struct file

---
+++ Authorization/Soursces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Authorization
+//
+//  Created by Пётр  on 19.01.2023.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+}
+
