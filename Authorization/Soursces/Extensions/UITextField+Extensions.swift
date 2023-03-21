8ecd7b932ee11d36ade4e0cbf72969e701124d82
Gynia_xd <Gynia_xd@mail.ru>
2023-03-21 21:26:33 +0300

    all commit

--- Authorization/Soursces/Extensions/UITextField+Extensions.swift
+++ Authorization/Soursces/Extensions/UITextField+Extensions.swift
@@ -9,12 +9,20 @@ import UIKit
 
 extension UITextField {
     func setLeftIcon(_ image: UIImage) {
-        let iconView = UIImageView(frame: CGRect(x: 10, y: 5, width: 20, height: 20))
+        let iconView = UIImageView(frame: CGRect(x: 30, y: 5, width: 20, height: 20))
         iconView.image = image
-        let iconConteinerView: UIView = UIView(frame: CGRect(x: 20, y: 0, width: 30, height: 30))
+        let iconConteinerView: UIView = UIView(frame: CGRect(x: 20, y: 0, width: 60, height: 30))
         iconConteinerView.addSubview(iconView)
         leftView = iconConteinerView
         leftViewMode = .always
+    }
+    func setRightIcon(_ image: UIImage) {
+        let iconView = UIImageView(frame: CGRect(x: 20, y: 5, width: 20, height: 20))
+        iconView.image = image
+        let iconConteinerView: UIView = UIView(frame: CGRect(x: 20, y: 0, width: 60, height: 30))
+        iconConteinerView.addSubview(iconView)
+        rightView = iconConteinerView
+        rightViewMode = .always
     }
 }
 
--- Authorization/Soursces/ViewController.swift
+++ Authorization/Soursces/ViewController.swift
@@ -22,54 +22,58 @@ class ViewController: UIViewController {
         return loginButton
     } ()
     
-   /* private lazy var facebookButton: UIButton = {
-        let facebookButton = UIButton()
+    private lazy var facebookButton: UIButton = {
+        let facebookButton = UIButton(type: .custom)
+        facebookButton.setImage(UIImage(systemName: "bonjour"), for: .normal)
+        facebookButton.imageView?.tintColor = .white
+        facebookButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: -20, bottom: 0, right: 0)
         facebookButton.setTitle("Facebook", for: .normal)
         facebookButton.setTitleColor(UIColor.white, for: .normal)
-        facebookButton.backgroundColor = .blue
         facebookButton.clipsToBounds = true
         facebookButton.layer.cornerRadius = 20
+        facebookButton.layer.borderWidth = 1
+        facebookButton.layer.borderColor = UIColor.blue.cgColor
+        facebookButton.backgroundColor = .blue
         facebookButton.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
-        facebookButton.translatesAutoresizingMaskIntoConstraints = false
         return facebookButton
-    } ()
-    */
-    private lazy var facebookButton: UIButton = {
-            let facebookButton = UIButton(type: .custom)
-            facebookButton.setImage(UIImage(systemName: "bonjour"), for: .normal)
-            facebookButton.imageView?.tintColor = .black
-            facebookButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: -20, bottom: 0, right: 0)
-            facebookButton.setTitle("Facebook", for: .normal)
-            facebookButton.setTitleColor(UIColor.black, for: .normal)
-            facebookButton.clipsToBounds = true
-            facebookButton.layer.cornerRadius = 20
-            facebookButton.layer.borderWidth = 1
-            facebookButton.layer.borderColor = UIColor.black.cgColor
-            facebookButton.backgroundColor = .clear
-            facebookButton.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
-            return facebookButton
-        }()
+    }()
     
     private lazy var twiterButton: UIButton = {
         let twiterButton = UIButton(type: .custom)
-        twiterButton.setImage(UIImage(systemName: "person.wave.2"), for: .normal)
+        twiterButton.setImage(UIImage(systemName: "network"), for: .normal)
+        twiterButton.imageView?.tintColor = .white
         twiterButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: -20, bottom: 0, right: 0)
-        twiterButton.imageView?.tintColor = .red
         twiterButton.setTitle("Twiter", for: .normal)
         twiterButton.setTitleColor(UIColor.white, for: .normal)
-        twiterButton.backgroundColor = .blue
         twiterButton.clipsToBounds = true
         twiterButton.layer.cornerRadius = 20
+        twiterButton.layer.borderWidth = 1
+        twiterButton.layer.borderColor = UIColor.blue.cgColor
+        twiterButton.backgroundColor = .blue
         twiterButton.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
-        twiterButton.translatesAutoresizingMaskIntoConstraints = false
         return twiterButton
     } ()
+    
+    private lazy var loginTextField: UITextField = {
+        let loginTextField = UITextField()
+        loginTextField.placeholder = "admin_users"
+        if let image = UIImage(systemName: "person") {
+            loginTextField.setLeftIcon(image)
+        }
+        if let image = UIImage(systemName: "checkmark.circle.fill") {
+            loginTextField.setRightIcon(image)
+        }
+        loginTextField.clipsToBounds = true
+        loginTextField.layer.cornerRadius = 20
+        loginTextField.backgroundColor = .lightGray
+        return loginTextField
+    }()
     
     private lazy var passwordTextField: UITextField = {
         let passwordTextField = UITextField()
         passwordTextField.placeholder = "Password"
         if let image = UIImage(systemName: "square.and.arrow.up") {
-        passwordTextField.setLeftIcon(image)
+            passwordTextField.setLeftIcon(image)
         }
         passwordTextField.clipsToBounds = true
         passwordTextField.layer.cornerRadius = 20
@@ -77,34 +81,58 @@ class ViewController: UIViewController {
         return passwordTextField
     } ()
     
- 
     
-    private lazy var loginTextField: UITextField = {
-        let loginTextField = UITextField()
-        loginTextField.placeholder = "Login"
-
-        loginTextField.clipsToBounds = true
-        loginTextField.layer.cornerRadius = 20
-        loginTextField.backgroundColor = .lightGray
-        return loginTextField
-    }()
+    
     
     private lazy var loginLabel: UILabel = {
         let loginLabel = UILabel()
         loginLabel.text = "Login"
+        loginLabel.textColor = .white
+        loginLabel.font = UIFont.boldSystemFont(ofSize: 34)
         return loginLabel
     }()
     
-    private lazy var connectView: UIView = {
-        let connectView = UIView()
-        connectView.layer.cornerRadius = 20
-        connectView.backgroundColor = .black
-        return connectView
+    private lazy var dontLabel: UILabel = {
+        let dontLabel = UILabel()
+        dontLabel.text = "Dont have account ?  Sign up"
+        dontLabel.textColor = .white
+        dontLabel.font = UIFont.boldSystemFont(ofSize: 14)
+        return dontLabel
     }()
-     
+    
+    private lazy var forgetLabel: UILabel = {
+        let forgetLabel = UILabel()
+        forgetLabel.text = "Forget your password ?"
+        forgetLabel.textColor = .white
+        forgetLabel.font = UIFont.boldSystemFont(ofSize: 14)
+        return forgetLabel
+    }()
+    
+    private lazy var connectLabel: UILabel = {
+        let connectLabel = UILabel()
+        connectLabel.text = "or connect with"
+        connectLabel.textColor = .white
+        connectLabel.font = UIFont.boldSystemFont(ofSize: 14)
+        return connectLabel
+    }()
+    
+    private lazy var leftConnectView: UIView = {
+        let leftConnectView = UIView()
+        leftConnectView.layer.cornerRadius = 20
+        leftConnectView.backgroundColor = .black
+        return leftConnectView
+    }()
+    
+    private lazy var rightConnectView: UIView = {
+        let rightConnectView = UIView()
+        rightConnectView.layer.cornerRadius = 20
+        rightConnectView.backgroundColor = .black
+        return rightConnectView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        view.backgroundColor = .darkGray
         setupHierarchy()
         setupLayout()
     }
@@ -113,13 +141,39 @@ class ViewController: UIViewController {
         view.addSubview(passwordTextField)
         view.addSubview(loginTextField)
         view.addSubview(loginButton)
-        view.addSubview(connectView)
+        view.addSubview(loginLabel)
+        view.addSubview(dontLabel)
+        view.addSubview(forgetLabel)
+        view.addSubview(connectLabel)
+        view.addSubview(leftConnectView)
+        view.addSubview(rightConnectView)
         view.addSubview(facebookButton)
         view.addSubview(twiterButton)
         
     }
     
     func setupLayout() {
+        
+        loginLabel.snp.makeConstraints { make in
+            make.centerX.equalTo(view)
+            make.top.equalTo(view).offset(150)
+        }
+        
+        dontLabel.snp.makeConstraints { make in
+            // make.left.equalTo(view).offset(100)
+            make.centerX.equalTo(view)
+            make.top.equalTo(facebookButton.snp.bottom).offset(30)
+            //make.right.equalTo(view.snp.centerX).offset(-20)
+        }
+        
+        forgetLabel.snp.makeConstraints { make in
+            make.centerX.equalTo(view)
+            make.top.equalTo(loginButton.snp.bottom).offset(30)
+            //make.right.equalTo(view.snp.centerX).offset(-20)
+        }
+        
+        
+        
         passwordTextField.snp.makeConstraints { make in
             make.centerX.equalTo(view)
             make.centerY.equalTo(view).offset(-20)
@@ -143,10 +197,25 @@ class ViewController: UIViewController {
             make.top.equalTo(passwordTextField.snp.bottom).offset(30)
         }
         
-        connectView.snp.makeConstraints { make in
+        leftConnectView.snp.makeConstraints { make in
+            make.height.equalTo(2)
+            make.width.equalTo(400)
+            make.left.equalTo(view).offset(50)
+            make.top.equalTo(loginButton.snp.bottom).offset(120)
+            make.right.equalTo(view.snp.centerX).offset(-50)
+        }
+        
+        connectLabel.snp.makeConstraints { make in
             make.centerX.equalTo(view)
+            make.top.equalTo(loginButton.snp.bottom).offset(110)
+            //make.right.equalTo(view.snp.centerX).offset(-20)
+        }
+        
+        rightConnectView.snp.makeConstraints { make in
             make.height.equalTo(2)
             make.width.equalTo(290)
+            make.left.equalTo(view.snp.centerX).offset(50)
+            make.right.equalToSuperview().offset(-50)
             make.top.equalTo(loginButton.snp.bottom).offset(120)
         }
         
